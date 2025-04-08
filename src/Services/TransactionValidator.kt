3ca03c44d816ed85ddd5b87a4e1b4ca2e6d8e32e
Eman Abobakr <eman.abobakr@vodafone.com>
2025-04-08 19:51:16 +0200

    task/add transaction validator logic

--- src/Services/TransactionValidator.kt
+++ src/Services/TransactionValidator.kt
@@ -1,17 +1,28 @@
 package Services
 
 import Models.UITransaction
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class TransactionValidator {
     fun isValidTransaction (transaction: UITransaction): Boolean {
-        return false
+        return checkAmount(transaction.amount) && checkDate(transaction.date)
     }
 
     private fun checkAmount(amount: Double): Boolean {
-        return false
+        return amount > 0
     }
 
     private fun checkDate(date: String): Boolean {
-        return false
+        try {
+            val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+            val localDate = LocalDate.parse(date, formatter)
+            if (!formatter.format(localDate).equals(date) || localDate > LocalDate.now()) {
+                return false
+            }
+            return true
+        } catch (e: Exception) {
+            return false
+        }
     }
 }
