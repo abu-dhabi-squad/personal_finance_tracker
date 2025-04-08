59b6f1d4d9e0c1287275bbf161599be250700c70
Amr Ashraf <AmrAshraf_inf@outlook.com>
2025-04-08 18:33:59 +0200

    add test cases for the category service implementation

--- src/Test/CategoryTests.kt
+++ src/Test/CategoryTests.kt
@@ -1,3 +1,46 @@
 package Test
 
-// Add test functions for every feature and call it in main test file
+import Models.Category
+import Services.CategoryService
+
+fun main() {
+    val categoryTest = CategoryTests()
+    categoryTest.testAddCategory()
+}
+class CategoryTests {
+
+
+    fun testAddCategory() {
+        val category = CategoryService()
+
+        test("check add new category", true,
+            category.addCategory(Category("Shopping")))
+
+        test("check if a new category is empty", false,
+            category.addCategory(Category("")))
+
+        test("check if the category has an invalid char", false,
+            category.addCategory(Category("Re^%nt")))
+
+    }
+
+
+    fun testDeleteCategory() {
+        val category: CategoryService = CategoryService()
+        val categorySize=category.getAllCategories().size
+
+        test("check if the category deleted", true,
+            category.deleteCategory(0))
+
+        test("check if the index is out of range", false,
+            category.deleteCategory(categorySize))
+
+    }
+
+    //Todo
+    fun testGetAllCategory() {
+        //val category: CategoryService = CategoryService()
+
+
+    }
+}
