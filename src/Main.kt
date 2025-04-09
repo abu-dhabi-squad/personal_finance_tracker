7b0c186c609bf01afe9c0c778223b02310e0d7f8
Amr Ashraf <AmrAshraf_inf@outlook.com>
2025-04-09 16:11:45 +0200

    add category validatations in a seprate class

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,19 @@
-fun main() {
+import Models.Category
+import Services.CategoryService
 
+fun main() {
+}
+
+fun showCategoriesMenu(categories:List<Category>): String {
+    var menuText = "categories: -\n"
+    if (categories.isNotEmpty()) {
+
+        categories.forEachIndexed { index, category ->
+            menuText += "${index + 1}) ${category.name}\n"
+        }
+    }
+    else {
+        menuText+="The category is empty!"
+    }
+    return menuText
 }
--- src/Services/CategoryService.kt
+++ src/Services/CategoryService.kt
@@ -1,18 +1,18 @@
 package Services
 
 import Models.Category
+import src.Services.CategoryValidator
 
 class CategoryService {
 
     private val categories: MutableList<Category> = mutableListOf()
 
     fun addCategory(category: Category): Boolean {
-        val trimmedCategory = category.name.trim()
-        if (trimmedCategory.isEmpty()) return false
 
-        trimmedCategory.lowercase().forEach {
-            if (it !in 'a'..'z') return false
-        }
+        val trimmedCategory = category.name.trim()
+        val isValidCategory=CategoryValidator.isValidCategoryName(trimmedCategory)
+        if (!isValidCategory)
+            return false
 
         return categories.add(Category(trimmedCategory))
     }
@@ -26,21 +26,9 @@ class CategoryService {
     }
 
     fun getAllCategories(): List<Category> {
-        return categories
+        return categories.toList()
     }
 
-    fun showCategoriesMenu(): String {
-        var menuText = "categories: -\n"
-        if (categories.isNotEmpty()) {
 
-            categories.forEachIndexed { index, category ->
-                menuText += "${index + 1}) ${category.name}\n"
-            }
-        }
-        else {
-            menuText+="The category is empty!"
-        }
-        return menuText
-    }
 
 }
