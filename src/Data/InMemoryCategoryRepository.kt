c128b2768e519e4462e61da741d01f07cf0272dd
Amr Ashraf <AmrAshraf_inf@outlook.com>
2025-04-09 19:43:40 +0200

    add validator class

--- src/Data/InMemoryCategoryRepository.kt
+++ src/Data/InMemoryCategoryRepository.kt
@@ -1,17 +1,21 @@
 package src.Data
 
 import Models.Category
+import src.Services.CategoryValidator
 
 class InMemoryCategoryRepository: ICategoryRepository {
     private val categories: MutableList<Category> = mutableListOf()
 
     override fun add(category: Category): Boolean {
         val trimmedCategory = category.name.trim()
-        if (trimmedCategory.isEmpty()) return false
-
-        trimmedCategory.lowercase().forEach {
-            if (it !in 'a'..'z') return false
-        }
+        val isValidCategory= CategoryValidator.isValidCategoryName(trimmedCategory)
+        if (!isValidCategory)
+            return false
+//        if (trimmedCategory.isEmpty()) return false
+//
+//        trimmedCategory.lowercase().forEach {
+//            if (it !in 'a'..'z') return false
+//        }
 
         return categories.add(Category(trimmedCategory))
     }
--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,25 @@
 import Models.Category
 import Services.CategoryService
 import src.Data.InFileCategoryRepository
+import src.Data.InMemoryCategoryRepository
 import java.io.File
 
 fun main() {
+
+    //val categoryServiceInMemory=CategoryService(InMemoryCategoryRepository())
+
+    val categoryServiceInFile=CategoryService(InFileCategoryRepository(File("category")))
+
+//    categoryService.addCategory(Category("Shopping"))
+//    categoryService.addCategory(Category("Shopping"))
+//    categoryService.addCategory(Category("Shopping"))
+    categoryServiceInFile.addCategory(Category("Shopping"))
+    categoryServiceInFile.addCategory(Category("rent"))
+    println(categoryServiceInFile.getAllCategories())
+
+
+    println(showCategoriesMenu(categoryServiceInFile.getAllCategories()))
+
 
 }
 
