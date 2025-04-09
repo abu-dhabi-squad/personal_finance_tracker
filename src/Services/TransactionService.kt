f5b3eea11a10e2e3dc0377035268b83b4cb4c079
Eslam <eslam.mohamed.551999@gmail.com>
2025-04-09 17:23:20 +0200

    update category, Transaction models to be serializable add transaction interface implement transaction interface with 2 types, memory & file move delete logic from transaction service add test cases to delete_transaction from file add logic of delete_transaction from file

--- src/Services/TransactionService.kt
+++ src/Services/TransactionService.kt
@@ -1,5 +1,6 @@
 package Services
 
+import Data.ITransactionRepository
 import Models.Category
 import Models.Transaction
 import Models.TransactionType
@@ -11,6 +12,7 @@ import java.util.*
 class TransactionService(
     private val transactions: MutableList<Transaction> = mutableListOf(),
     private var balance: Double = 0.0,
+    private val transactionRepo: ITransactionRepository,
 ) {
 
     fun getTransactionsSize(): Int = transactions.size
@@ -136,24 +138,8 @@ class TransactionService(
         }
     }
 
-
-    fun isTransactionExists(transaction: Transaction): Boolean {
-        return transactions.contains(transaction)
-    }
-
     fun deleteTransaction(transaction: Transaction): Boolean {
-        if (isTransactionExists(transaction)) {
-            if (transaction.transactionType == TransactionType.INCOME && !canWithdraw(transaction.amount)) return false
-            val success = transactions.remove(transaction)
-            if (success) {
-                when(transaction.transactionType) {
-                    TransactionType.INCOME -> balance -= transaction.amount
-                    TransactionType.EXPENSES -> balance += transaction.amount
-                }
-                return true
-            }
-        }
-        return false
+        return transactionRepo.delete(transaction)
     }
 
     fun listAllTransactions(): List<Transaction> {
