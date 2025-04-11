def286dd2f0e3ae95333a2278f78880a903614b3
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-04-11 19:28:02 +0200

    remove unused imports

--- src/Services/ReportValidatorImplementation.kt
+++ src/Services/ReportValidatorImplementation.kt
@@ -1,11 +1,9 @@
 package Services
 
-import Models.Category
-import Models.TransactionType
-import src.Data.CategoryInterface
 import java.time.LocalDate
 
 class ReportValidatorImplementation: ReportValidatorInterface {
     override fun isValidYear(year: Int): Boolean = ( year in 1900..LocalDate.now().year)
+
     override fun isValidMonth(month: Int) :Boolean =  (month in 1..12)
 }
--- src/Services/ReportValidatorInterface.kt
+++ src/Services/ReportValidatorInterface.kt
@@ -1,8 +1,5 @@
 package Services
 
-import Models.Category
-import Models.TransactionType
-
 interface ReportValidatorInterface {
     fun isValidYear(year: Int):Boolean
     fun isValidMonth(month: Int):Boolean
