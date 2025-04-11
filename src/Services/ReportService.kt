1e316ad03ff67582db227881810f9e385d23c744
NourElhoda Ahmed <nourelhodaahmed208@gmail.com>
2025-04-11 16:31:42 +0200

    add implementation for get summary by category name

--- src/Services/ReportService.kt
+++ src/Services/ReportService.kt
@@ -35,7 +35,21 @@ class ReportService(private val transactionInterface: TransactionInterface, priv
     }
 
     fun getSummaryByCategory(category: Category): SummaryTransactions {
+        if (reportValidatorInterface.isValidcategory(category)) {
+            val transactions = transactionInterface.getAll()
+            var totalIncome = 0.0
+            var totalExpenses = 0.0
 
+            val transactionsRes = transactions.filter { transaction -> transaction.category.name == category.name }
+
+            transactionsRes.forEach { transaction ->
+                if (transaction.transactionType == TransactionType.INCOME)
+                    totalIncome += transaction.amount
+                else totalExpenses += transaction.amount
+            }
+
+            return SummaryTransactions( totalIncome, totalExpenses, transactionsRes)
+        }
         return SummaryTransactions(0.0, 0.0, listOf())
     }
 
